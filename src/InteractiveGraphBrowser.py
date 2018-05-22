ad227895eca1525aed8c97d75bdff46f2eac4ea7
Tairan Liu <liutairan2012@gmail.com>
2018-05-22 15:18:06 -0500

    InteractiveGraphBrowser changes

--- src/InteractiveGraphBrowser.py
+++ src/InteractiveGraphBrowser.py
@@ -1,6 +1,100 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebEngineWidgets import *
-from PyQt5.QtGui import *
-import sip
 import sys
+import os
+import string
+from random import *
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QPushButton,
+    QListWidget, QListWidgetItem, QAbstractItemView, QWidget, QAction,
+    QTabWidget, QTableWidget, QTableWidgetItem, QVBoxLayout, QHBoxLayout,
+    QHeaderView, QLabel, QTreeWidget, QTreeWidgetItem, QToolBar, QLineEdit,
+    QCompleter, QSizePolicy, QComboBox, QMessageBox, QDialog, QDialogButtonBox)
+from PyQt5.QtGui import QIcon, QPainter
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QStringListModel, QRect, QSize, Qt
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib.pyplot as plt
+
+import numpy as np
+import random
+# from PyQt5.QtCore import *
+# from PyQt5.QtWidgets import *
+# from PyQt5.QtWebEngineWidgets import *
+# from PyQt5.QtGui import *
+import sip
+
+class InteractiveGraphBrowser(QDialog):
+    resized = pyqtSignal()
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.title = 'Relation Graph'
+        self.left = 0
+        self.top = 0
+        self.width = 480
+        self.height = 300
+        self.toolbarheight = 65
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.centerWindow()
+        self.bundle_dir = os.path.dirname(os.path.abspath(__file__))
+        self.initWidgets()
+        self.setFocus()
+        #print(self.focusWidget())
+
+    def centerWindow(self):
+        frameGeo = self.frameGeometry()
+        screen = QApplication.desktop().screenNumber(QApplication.desktop().cursor().pos())
+        centerPoint = QApplication.desktop().screenGeometry(screen).center()
+        frameGeo.moveCenter(centerPoint)
+        self.move(frameGeo.topLeft())
+
+    def initWidgets(self):
+        self.figure = plt.figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar.setVisible(False)
+        # Just some button connected to `plot` method
+        self.button = QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+
+        # set the layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        self.setLayout(layout)
+
+        #self.show()
+
+    def plot(self):
+        ''' plot some random stuff '''
+        # random data
+        data = [random.random() for i in range(10)]
+
+        # instead of ax.hold(False)
+        self.figure.clear()
+
+        # create an axis
+        ax = self.figure.add_subplot(111)
+
+        # discards the old graph
+        # ax.hold(False) # deprecated, see above
+
+        # plot data
+        ax.plot(data, '*-')
+        line, = ax.plot(np.random.rand(100), 'o', picker=5)
+        ax.axis('off')
+        self.figure.canvas.mpl_connect('pick_event', self.onpick)
+
+        # refresh canvas
+        self.canvas.draw()
+
+    def onpick(self, event):
+        thisline = event.artist
+        xdata = thisline.get_xdata()
+        ydata = thisline.get_ydata()
+        ind = event.ind
+        points = tuple(zip(xdata[ind], ydata[ind]))
+        print('onpick points:', points)
