b903165cc4397952fac3605825c1135242042c48
Tairan Liu <liutairan2012@gmail.com>
2018-05-29 15:30:44 -0500

    Initiate ReferenceStructure class

---
+++ src/ReferenceStructure.py
@@ -0,0 +1,60 @@
+class Article:
+    def __init__(self, author="", title="", journal="", volume="", number="", year="", abstract=""):
+        self.data = {"author":author,
+                     "title":title,
+                     "journal":journal,
+                     "volume":volume,
+                     "number":number,
+                     "year":year,
+                     "abstract":abstract}
+
+class Book:
+    def __init__(self):
+        self.data = None
+class Booklet:
+    def __init__(self):
+        self.data = None
+
+class Conference:
+    def __init__(self):
+        self.data = None
+
+class Inbook:
+    def __init__(self):
+        self.data = None
+
+class Incollection:
+    def __init__(self):
+        self.data = None
+
+class Inproceedings:
+    def __init__(self):
+        self.data = None
+
+class Manual:
+    def __init__(self):
+        self.data = None
+
+class Mastersthesis:
+    def __init__(self):
+        self.data = None
+
+class Misc:
+    def __init__(self):
+        self.data = None
+
+class Phdthesis:
+    def __init__(self):
+        self.data = None
+
+class Proceedings:
+    def __init__(self):
+        self.data = None
+
+class Techreport:
+    def __init__(self):
+        self.data = None
+
+class Unpublished:
+    def __init__(self):
+        self.data = None
