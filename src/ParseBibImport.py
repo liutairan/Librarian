856e3816e6effec67e77d8e1e6bbc3a28e4af57d
Tairan Liu <liutairan2012@gmail.com>
2018-06-16 00:22:49 -0500

    Minor change

--- src/ParseBibImport.py
+++ src/ParseBibImport.py
@@ -35,10 +35,27 @@ class BibTeXParser():
         else:
             print("No valid BibTeX item found.")
         for pair in indPairs:
-            self.parseBibString(''.join(data[pair[0]:pair[1]]) )
+            self.parseBibItem(data[pair[0]:pair[1]] )
 
-    def parseBibString(self, bibStr):
-        pass
+    def parseBibItem(self, bibItemList):
+        bibType = "article"
+        ind1 = bibItemList[0].index('@')
+        ind2 = bibItemList[0].index('{')
+        ind3 = bibItemList[0].index(",\n")
+        bibType = bibItemList[0][ind1+1:ind2].replace(" ", "")
+        citeKey = bibItemList[0][ind2+1:ind3]
+        for line in bibItemList[1:]:
+            if "=" in line and "\n" in line:
+                tempInd1 = line.index("=")
+                fieldKey = line[0:tempInd1].replace(" ", "")
+                tempInd2 = line.index("{")
+                tempInd3 = tempInd2
+                if "},\n" in line:
+                    tempInd3 = line.index("},\n")
+                elif "}\n" in line:
+                    tempInd3 = line.index("}\n")
+                fieldValue = line[tempInd2+1:tempInd3]
+                print([fieldKey,fieldValue])
 
 if __name__ == "__main__":
     bp = BibTeXParser("My Collection.bib")
