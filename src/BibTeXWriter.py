70c1c72279141f490949e7c34f7198e6a216dfcd
Tairan Liu <liutairan2012@gmail.com>
2018-06-16 16:36:13 -0500

    Init BibTeXWriter class

---
+++ src/BibTeXWriter.py
@@ -0,0 +1,8 @@
+class BibTeXWriter:
+    def __init__(self, path, refDictList):
+        self.path = path
+        self.referenceDictList = refDictList
+        self.writeFile()
+
+    def writeFile(self):
+        pass
