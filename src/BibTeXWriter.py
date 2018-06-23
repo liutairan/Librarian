fdaa2012b6d1819c0e54c6f08c48092d2ceba602
Tairan Liu <liutairan2012@gmail.com>
2018-06-22 21:11:43 -0500

    Export as bibtex file

--- src/BibTeXWriter.py
+++ src/BibTeXWriter.py
@@ -21,6 +21,21 @@ class BibTeXWriter:
         print(refItem)
         bibItemStr = ""
         if refItem['Type'].lower() in BibTeXTypes:
-            bibItemStr = bibItemStr + "@" + refItem['Type'].lower() + "{" + "\n"
+            bibtexType = refItem['Type'].lower()
+            fieldSet = ReferenceStandardStructure[bibtexType]
+            citeKey = ""
+            if 'Citekey' in refItem:
+                if  len(refItem['Citekey']):
+                    citeKey = refItem['Citekey']
+            if len(citeKey) == 0:
+                if len(refItem['Authors']):
+                    tempS1 = refItem['Authors'].split(",")[0]
+                    citeKey = citeKey + tempS1
+                    tempS2 = str(refItem['Year'])
+                    citeKey = citeKey + tempS2
+                    if len(refItem['Title']):
+                        tempS3 = refItem['Title'].split(" ")[0]
+                        citeKey = citeKey + tempS3
+            bibItemStr = bibItemStr + "@" + refItem['Type'].lower() + "{" + citeKey + ",\n"
             bibItemStr = bibItemStr + "}\n"
         return bibItemStr
