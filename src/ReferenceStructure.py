768a17c3067f8b1b44225837a9adbb179df3b084
Tairan Liu <liutairan2012@gmail.com>
2018-06-24 22:28:20 -0500

    Remove old reference structure

--- src/ReferenceStructure.py
+++ src/ReferenceStructure.py
@@ -25,7 +25,7 @@ MiscFieldSet = set(["author"])
 PhdthesisFieldSet = set(["author"])
 ProceedingsFieldSet = set(["author"])
 TechreportFieldSet = set(["author"])
-UnpublishedFieldSet = set(["author"])
+UnpublishedFieldSet = set(["author", "title", "note", "month", "year"])
 
 ReferenceStandardStructure = {'article':         ArticleFieldSet,
                               'book':            BookFieldSet,
@@ -41,67 +41,3 @@ ReferenceStandardStructure = {'article':         ArticleFieldSet,
                               'techreport':      TechreportFieldSet,
                               'unpublished':     UnpublishedFieldSet
                              }
-
-class Article:
-    def __init__(self, author="", title="", journal="", volume="", number="", year="", pages="", month="", note="", abstract=""):
-        self.data = {"author"    :author,
-                     "title"     :title,
-                     "journal"   :journal,
-                     "volume"    :volume,
-                     "number"    :number,
-                     "year"      :year,
-                     "pages"     :pages,
-                     "month"     :month,
-                     "note"      :note,
-                     "abstract"  :abstract}
-
-class Book:
-    def __init__(self):
-        self.data = None
-class Booklet:
-    def __init__(self):
-        self.data = None
-
-class Conference:
-    def __init__(self):
-        self.data = None
-
-class Inbook:
-    def __init__(self):
-        self.data = None
-
-class Incollection:
-    def __init__(self):
-        self.data = None
-
-class Inproceedings:
-    def __init__(self):
-        self.data = None
-
-class Manual:
-    def __init__(self):
-        self.data = None
-
-class Mastersthesis:
-    def __init__(self):
-        self.data = None
-
-class Misc:
-    def __init__(self):
-        self.data = None
-
-class Phdthesis:
-    def __init__(self):
-        self.data = None
-
-class Proceedings:
-    def __init__(self):
-        self.data = None
-
-class Techreport:
-    def __init__(self):
-        self.data = None
-
-class Unpublished:
-    def __init__(self):
-        self.data = None
