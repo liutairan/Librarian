6551fbcd6e1498d81b4ae8c219a37c6d5be4546a
Tairan Liu <liutairan2012@gmail.com>
2019-06-11 09:55:48 -0500

    Newest update

--- src/GoogleScholarSearch.py
+++ src/GoogleScholarSearch.py
@@ -13,6 +13,11 @@ from bs4 import BeautifulSoup
 unicode = str
 encode = lambda s: unicode(s)
 
+class GoogleScholarConfig:
+    MaxNumPerPage = 10
+    BaseSite = 'http://scholar.google.com'
+    UserAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:42.0) Gecko/20100101 Firefox/42.0'
+
 def main():
     pass
 
